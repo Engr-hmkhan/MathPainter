c1d8a4848a9a93f951fa5b7bc167aefa5d8214d4
Hafiz Muhammad Mutahhar Khan <Engr-hmkhan@users.noreply.github.com>
2024-09-07 07:22:14 +0500

    add a comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
 from PIL import Image
 
-# Creating 3D Numpy arrays of  zeros and then rep;ace zeros with yellow pixels
+# Creating 3D Numpy arrays of  zeros and then rep;ace zeros with yellow pixels.
 
