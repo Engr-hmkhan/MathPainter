04085d81d0ca07e8679f75452170c2f457b4ac0c
Hafiz Muhammad Mutahhar Khan <Engr-hmkhan@users.noreply.github.com>
2024-09-07 07:45:21 +0500

    added red color in the canvas

--- main.py
+++ main.py
@@ -7,5 +7,10 @@ data = np.zeros((5, 4, 3), dtype=np.uint8)
 data[:] = [255, 255, 0] # 255 represent full red, green, blue # 0 represent black
 print(data)
 
+# make a red patch
+data[1:3] = [255, 0, 0]
+print(data)
+
 img = Image.fromarray(data, 'RGB')
 img.save('canvas.png')
+
